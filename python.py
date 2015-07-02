d153b2f3c7bbe89e74eb27d42e321bf365f9365c
Goshine <greenlig@gmail.com>
2015-07-02 07:58:36 +0100

    add parms and fix input

--- python.py
+++ python.py
@@ -1,14 +1,14 @@
 #-----------#-------------
 
-def harmonic ():
+def harmonic (n):
     #Compute the sum of 1/k for k = 1 to n
-    total = 0
-    for k in range (1, n+1):
-        total += 1 / k
+    total = 0.0
+    for k in range (1, n + 1):
+        total += 1.0 / k
     return total
 
 def _main ():
-    n = int (int("Enter a positive integer "))
+    n = int (input("Enter a positive integer "))
     print ("The sum of 1/k for k = 1 to ", n , "is", harmonic(n))
 
 _main()
